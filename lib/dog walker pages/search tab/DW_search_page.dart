c49a599b26f3c085ac864a7c315059e329fc9373
josephassaf999 <josephassaf111@gmail.com>
2025-04-11 01:56:00 +0300

    fixed search page for dog walker

--- lib/dog walker pages/search tab/DW_search_page.dart
+++ lib/dog walker pages/search tab/DW_search_page.dart
@@ -2,24 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../info page/DW_info_page.dart';
 
-// Dog class
+// Dog model class
 class Dog {
   final String name;
-  final String location;
-  final String description;
-  final List<String> imageUrls;
-  final String phone_number;
   final String breed;
   final String age;
+  final List<String> imageUrls;
+  final String location;
+  final String phoneNumber;
+  final String description;
+  final String ownerId;
 
   Dog({
     required this.name,
-    required this.location,
-    required this.description,
-    required this.imageUrls,
-    required this.phone_number,
     required this.breed,
     required this.age,
+    required this.imageUrls,
+    required this.location,
+    required this.phoneNumber,
+    required this.description,
+    required this.ownerId,
   });
 }
 
@@ -32,153 +34,151 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _searchController = TextEditingController();
-
-  // Perform search for dogs in Firestore
-  void _performSearch(String query) {
-    // Clear previous search results
-    _searchResults.clear();
-
-    // Search Firestore for dogs
-    FirebaseFirestore.instance
-        .collection('Dogs') // Ensure it's the 'Dogs' collection
-        .where('name', isGreaterThanOrEqualTo: query)
-        .where('name', isLessThan: query + 'z')
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      setState(() {
-        _searchResults = querySnapshot.docs
-            .map((doc) => Dog(
-          name: doc['name'] as String,
-          location: doc['location'] as String,
-          description: doc['description'] as String,
-          imageUrls: List<String>.from(doc['imageUrls']),
-          phone_number: doc['phone_number'],
-          breed: doc['breed'],
-          age: doc['age'],
-        ))
-            .toList();
-      });
-    });
-  }
-
-  // Correct the type to List<Dog>
   List<Dog> _searchResults = [];
-
-  // Navigate to the InfoPage for the selected dog
-  void _navigateToInfoPage(BuildContext context, Dog dog) async {
-    List<String> imageUrls = await _getImageUrlsForItem(dog.name);
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => InfoPage(
-          name: dog.name,
-          location: dog.location,
-          imageUrls: imageUrls,
-          breed: dog.breed,
-          age: dog.age,
-          phone_number: dog.phone_number,
-          description: dog.description, ownerId: '',
-        ),
-      ),
-    );
-  }
-
-  // Fetch additional image URLs for a specific dog
-  Future<List<String>> _getImageUrlsForItem(String itemName) async {
-    List<String> assetPaths = [];
-
-    try {
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-          .collection('Dogs') // Ensure it's the 'Dogs' collection
-          .where('name', isEqualTo: itemName)
-          .get();
-
-      querySnapshot.docs.forEach((doc) {
-        if (doc.exists) {
-          List<dynamic> urls = doc['imageUrls'];
-          assetPaths.addAll(urls.map((url) => "$url"));
-        }
-      });
-    } catch (error) {
-      print("Error fetching image URLs: $error");
-    }
-
-    return assetPaths;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text(
-          'Search Dogs',  // Change the title to 'Search Dogs'
+          'Search Dogs',
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Colors.blue, // Set app bar color to black
-        automaticallyImplyLeading: false, // Remove the back arrow
+        backgroundColor: Colors.blue,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Search dogs by name or breed',  // Change hint text
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    String query = _searchController.text.trim();
-                    _performSearch(query);
-                  },
-                ),
-              ),
-              onChanged: (value) {
-                String query = value.trim();
-                _performSearch(query);
-              },
-            ),
-            const SizedBox(height: 16.0),
-            Expanded(
-              child: _searchResults.isNotEmpty
-                  ? ListView.builder(
-                itemCount: _searchResults.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final dog = _searchResults[index];
-                  return GestureDetector(
-                    onTap: () {
-                      _navigateToInfoPage(context, dog);
-                    },
-                    child: Card(
-                      elevation: 3.0,
-                      margin: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: ListTile(
-                        title: Text(
-                          dog.name,
-                          style: TextStyle(fontWeight: FontWeight.bold),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('Dog owner').get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+              return Column(
+                children: [
+                  // Search Bar
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Search dogs by name or breed',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.search),
+                          onPressed: () {
+                            String query = _searchController.text.trim();
+                            _performSearch(query);
+                          },
                         ),
-                        subtitle: Text(dog.location),
-                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      onChanged: (value) {
+                        String query = value.trim();
+                        _performSearch(query);
+                      },
+                    ),
+                  ),
+                  // Search Results List
+                  Expanded(
+                    child: _searchResults.isNotEmpty
+                        ? ListView.builder(
+                      itemCount: _searchResults.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final dog = _searchResults[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => InfoPage(
+                                  name: dog.name,
+                                  breed: dog.breed,
+                                  age: dog.age,
+                                  imageUrls: dog.imageUrls,
+                                  location: dog.location,
+                                  phone_number: dog.phoneNumber,
+                                  description: dog.description,
+                                  ownerId: dog.ownerId,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Card(
+                            elevation: 3.0,
+                            margin: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: ListTile(
+                              title: Text(dog.name),
+                              subtitle: Text(dog.location),
+                              trailing: Icon(Icons.arrow_forward_ios),
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                        : const Center(
+                      child: Text(
+                        'No search results',
+                        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                       ),
                     ),
-                  );
-                },
-              )
-                  : Center(
-                child: Text(
-                  'No search results',  // Update the message to 'No search results'
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
                   ),
-                ),
-              ),
-            ),
-          ],
-        ),
+                ],
+              );
+            } else {
+              return const Center(child: Text('No Dog Owners found'));
+            }
+          }
+        },
       ),
     );
   }
+  void _performSearch(String query) {
+    List<Dog> filteredDogs = [];
+
+    // Convert query to lowercase for case-insensitive search
+    query = query.toLowerCase();
+
+    // Fetch all Dog owner documents
+    FirebaseFirestore.instance.collection('Dog owner').get().then((ownersSnapshot) async {
+      List<Dog> allDogs = [];
+
+      // Loop through each dog owner
+      for (var ownerDoc in ownersSnapshot.docs) {
+        // Fetch dogs from each owner's 'dogs' subcollection
+        QuerySnapshot dogSnapshot = await ownerDoc.reference.collection('dogs').get();
+
+        // Loop through each dog and check if it matches the query
+        dogSnapshot.docs.forEach((doc) {
+          // Print document data for debugging
+          print("Dog data: ${doc.data()}");
+
+          String dogName = (doc['dog_name'] as String? ?? '').toLowerCase();
+          String dogBreed = (doc['breed'] as String? ?? '').toLowerCase();
+
+          // Check if either the name or breed matches the query
+          if (dogName.contains(query) || dogBreed.contains(query)) {
+            allDogs.add(Dog(
+              name: doc['dog_name'] ?? 'No Name',
+              breed: doc['breed'] ?? 'Unknown Breed',
+              age: doc['age']?.toString() ?? 'Unknown Age',
+              imageUrls: List<String>.from(doc['images'] ?? []),
+              location: doc['location'] ?? 'Unknown Location',
+              phoneNumber: doc['phone_number'] ?? '',
+              description: doc['description'] ?? 'No Description',
+              ownerId: ownerDoc.id,
+            ));
+          }
+        });
+      }
+
+      // Update the search results
+      setState(() {
+        _searchResults = allDogs;
+      });
+    }).catchError((e) {
+      print('Error fetching dogs: $e');
+    });
+  }
+
 }
